3b566d1ac406be85b76617b27d02683e8d969b6b
elliot-100 <3186037+elliot-100@users.noreply.github.com>
2023-09-21 12:01:26 +0100

    Remove useless testing

--- tests/test_imports.py
+++
@@ -1,8 +0,0 @@
-"""Trivial test that package imports OK."""
-
-import britishcycling_clubs as bc
-
-
-def test_spond_import():
-    """Won't get to this test if imports fail."""
-    assert "bc" in globals()
